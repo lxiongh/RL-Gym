d455e306c6c9f39a33c0b49b3b6988e11a3dec64
lxiongh <lxiongh@126.com>
2018-07-29 10:11:09 +0800

    add cartpole

---
+++ convert_mp4_to_gif.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+palette="/tmp/palette.png"
+
+filters="fps=5,scale=-1:-1:flags=lanczos"
+
+ffmpeg -v warning -i $1 -vf "$filters,palettegen=stats_mode=diff" -y $palette
+
+ffmpeg -i $1 -i $palette -lavfi "$filters,paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle" -y $2
